840dd2f53c1336c291559db806544619bf2b9b4d
DanyCatarino <catarino.dany@gmail.com>
2019-09-19 16:46:32 +0200

    Ajoutez des fichiers projet.

---
+++ ModelObjet/Film.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelObjet
+{
+    class Film
+    {
+        public string TitreFilm { get; set; }
+        public int NbEntrees { get; set; }
+        public string PhotoFilm { get; set; }
+
+    }
+}
---
+++ ModelObjet/Realisateur.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelObjet
+{
+    class Realisateur
+    {
+        public string NomRealisateur { get; set; }
+        public string PrenomRealisateur { get; set; }
+        public string PhotoRealisateur { get; set; }
+        
+    }
+}
